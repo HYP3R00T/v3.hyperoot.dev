1c412937dc0d9b8a2602e1958297ac71c6d51cb2
hyperoot <hyperoot.tech@proton.me>
2024-08-20 19:12:37 +0000

    content config fixed

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,18 +3,17 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: ({ image }) =>
-    z.object({
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
-      title: z.string(),
-      featured: z.boolean().optional().default(false),
-      draft: z.boolean().optional().default(false),
-      category: z.string(),
-      tags: z.array(z.string()).default([""]),
-      description: z.string(),
-    }),
+  schema: z.object({
+    author: z.string().default(SITE.author),
+    pubDatetime: z.date(),
+    modDatetime: z.date().optional().nullable(),
+    title: z.string(),
+    featured: z.boolean().optional().default(false),
+    draft: z.boolean().optional().default(false),
+    category: z.string(),
+    tags: z.array(z.string()).default([""]),
+    description: z.string(),
+  }),
 });
 
 export const collections = { blog };
