3be065799337e78588569aa12864d6fc0bd1d8b4
hyperoot <hyperoot.tech@proton.me>
2024-08-20 04:30:31 +0000

    astro icon added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      iconDir: "./src/assets/icons",
+    }),
+  ],
 });
