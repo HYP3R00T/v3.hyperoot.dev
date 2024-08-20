6ac6ed794e7b1542b4445ba7dec03c68cb07d5c6
hyperoot <hyperoot.tech@proton.me>
2024-08-20 04:31:50 +0000

    ui components added

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -13,9 +13,33 @@ export interface BaseLayoutProps {
 }
 
 // For social links (config.ts)
-export type SocialObjects = {
+export interface SocialObjects {
   name: string;
   href: string;
   active: boolean;
   linkTitle: string;
-}[];
+}
+[];
+
+export interface ExperienceProps {
+  job_title: string;
+  description: string;
+  duration: string;
+  tags?: string[];
+  url: string;
+}
+
+export interface ExpCardProps {
+  data: ExperienceProps;
+}
+
+export interface ProjectsProps {
+  name: string;
+  description: string;
+  tags?: string[];
+  url: string;
+}
+
+export interface ProjCardProps {
+  data: ProjectsProps;
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,29 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--primary)",
-        foreground: "var(--primary-foreground)",
+        background: "var(--darkest)",
+        foreground: "var(--white)",
         accent: "var(--accent)",
-        primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "var(--primary-foreground)",
-        },
-        secondary: {
-          DEFAULT: "var(--secondary)",
-          foreground: "var(--secondary-foreground)",
-        },
-        tertiary: {
-          DEFAULT: "var(--tertiary)",
-          foreground: "var(--tertiary-foreground)",
-        },
+        white: "var(--white)",
+        darkest: "var(--darkest)",
+        darker: "var(--darker)",
+        dark: "var(--dark)",
+        light: "var(--light)",
+        lighter: "var(--lighter)",
+        lightest: "var(--lightest)",
+        blurred: "var(--blurred)",
+        accenttransparent: "var(--accent-transparent)",
+        blob: "var(--blob)",
       },
+    },
+    fontFamily: {
+      sans: ["'Inter Variable'", ...defaultTheme.fontFamily.sans],
+      mono: ["'JetBrains Mono'", ...defaultTheme.fontFamily.mono],
     },
   },
   plugins: [],
