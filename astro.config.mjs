31eeacfec4bcde60106207c217503866d6fb99e9
hyperoot <hyperoot.tech@proton.me>
2024-08-25 06:45:45 +0000

    added supported for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from "rehype-external-links";
 import icon from "astro-icon";
+import AutoImport from "astro-auto-import";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,5 +30,14 @@ export default defineConfig({
     icon({
       iconDir: "./src/assets/icons",
     }),
+    AutoImport({
+      imports: [
+        // Import a component’s default export
+        // generates:
+        // import A from './src/components/A.astro';
+        // "./src/components/A.astro",
+      ],
+    }),
+    mdx(),
   ],
 });
